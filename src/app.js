dc5693e082c8b5558c8b497889f4a8feab6d8fa2
firstDemoRepo <180681461+abdu1lah26@users.noreply.github.com>
2025-12-20 11:35:35 +0530

    setup cloudinary, multer, controllers ant routes

--- src/app.js
+++ src/app.js
@@ -6,11 +6,17 @@ const app = express();
 
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
-    credentials: true 
+    credentials: true
 }))
-app.use(express.json({limit: '16kb'}));
+app.use(express.json({ limit: '16kb' }));
 app.use(express.urlencoded({ extended: true, limit: '16kb' }));
 app.use(cookieParser());
 app.use(express.static('public'));
+
+//route imports
+import userRoutes from './routes/user.routes.js';
+
+//route middlewares
+app.use('/api/v1/users', userRoutes);
 
 export { app }
